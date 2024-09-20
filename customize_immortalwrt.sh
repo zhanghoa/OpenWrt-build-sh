505f1dae57108f1ed713a0ef68a7f3872b4015b1
zhanghoa <42800990+zhanghoa@users.noreply.github.com>
2024-09-20 21:29:32 +0800

    Update customize_immortalwrt.sh

--- customize_immortalwrt.sh
+++ customize_immortalwrt.sh
@@ -28,7 +28,7 @@ sed -i 's/UTC/UTC-8/g' package/base-files/files/bin/config_generate
 
 # 9. 配置编译选项（可选）
 # 如果需要通过 menuconfig 进行额外配置，请取消注释以下行
-# make menuconfig
+make menuconfig
 
 # 10. 编译固件
 # make -j$(nproc)
